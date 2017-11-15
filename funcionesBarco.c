d0046407ae061bc3d4562b526113765c9f34c77a
AhiVieneElProfe <32778527+AhiVieneElProfe@users.noreply.github.com>
2017-11-15 13:48:56 -0600

    Create funcionesBarco.c

---
+++ funcionesBarco.c
@@ -0,0 +1,14 @@
+#ifndef funcionesBarco_h
+#define funcionesBarco_h
+
+#include <stdio.h>
+
+void bienvenida();
+
+void navegarAlNorte(int *x, int *y);
+
+void navegarAlSur(int *x, int *y);
+
+void imprimirPosicion(int x, int y);
+
+#endif /* funcionesBarco_h */
